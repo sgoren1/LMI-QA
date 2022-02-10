f9ee7a3af88a606a867ccdd32d297689da3ec557
ulb-yizhu <yi.zhu@ulb.be>
2022-02-10 21:38:08 +0100

    django part 3 done

--- Wiki_QA_System-master/mysite/LMI_NLP/urls.py
+++ Wiki_QA_System-master/mysite/LMI_NLP/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
 from . import views
+# < ... > get a part of the URL, before ":" is the type of the data got
+
+app_name = 'LMI_NLP'
 
 urlpatterns = [
+    # ex: /LMI_NLP/
     path('', views.index, name='index'),
+    # ex: /LMI_NLP/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /LMI_NLP/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /LMI_NLP/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- Wiki_QA_System-master/mysite/LMI_NLP/views.py
+++ Wiki_QA_System-master/mysite/LMI_NLP/views.py
@@ -1,6 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'LMI_NLP/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)  # get_list_or_404() similar
+    return render(request, 'LMI_NLP/detail.html', {'question': question})
+
+# try except written in hand
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'LMI_NLP/detail.html', {'question': question})
+
+# old detail function non used
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
