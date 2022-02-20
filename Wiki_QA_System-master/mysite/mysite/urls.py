d0ad1769b7dd27f4c77f3fc459053282eab18abb
ioudahya <44835548+ioudahya@users.noreply.github.com>
2022-02-20 16:35:23 +0100

    Add files via upload

--- Wiki_QA_System-master/mysite/mysite/urls.py
+++ Wiki_QA_System-master/mysite/mysite/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
 urlpatterns = [
     path('LMI_NLP/', include('LMI_NLP.urls')),
     path('admin/', admin.site.urls),
