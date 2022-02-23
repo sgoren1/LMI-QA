ae5300eb4b2e27d45b2731ca0363e837568196c0
ulb-yizhu <yi.zhu@ulb.be>
2022-02-23 13:40:44 +0100

    display site ok. but ugly !

--- Wiki_QA_System-master/mysite/LMI_NLP/views.py
+++ Wiki_QA_System-master/mysite/LMI_NLP/views.py
@@ -31,7 +31,6 @@ class RexanaMain(generic.ListView):
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
-
 class DetailView(generic.DetailView):  # detailView expect primary key,so change views question_id to pk
     model = Question
     template_name = 'LMI_NLP/detail.html'
@@ -89,18 +88,19 @@ def Execution(request):
         if form.is_valid():
             # cleaned data in form.cleaned_data
             top3contexts, top3predictions = Application.SiteMain(form.cleaned_data["question"])
-            q = Top3Results(top3contexts[0],top3contexts[1],top3contexts[2])
-            l = Top3Predition(top3predictions[0],top3predictions[1],top3predictions[2])
+            q = Top3Results(result_text1=top3contexts[0], result_text2=top3contexts[1], result_text3=top3contexts[2])
+            l = Top3Predition(result_texte1=top3predictions[0], result_texte2=top3predictions[1],
+                              result_texte3=top3predictions[2])
 
             q.save()
 
             l.save()
-            return HttpResponseRedirect('/LMI_NLP/')  # needs a page to add the responses
+            return render(request, 'LMI_NLP/Execution.html', {'q': q})
+            # return HttpResponseRedirect('/LMI_NLP/Execution.html')  # needs a page to add the responses
     else:
         form = QuestionForm()
     return render(request, 'LMI_NLP/Execution.html', {'form': form})
 
 
 def YourQuestion(request):
-    return render(request,'LMI_NLP/your-question.html')
-
+    return render(request, 'LMI_NLP/your-question.html')
