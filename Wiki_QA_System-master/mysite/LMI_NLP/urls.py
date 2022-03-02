4f7ae4f3ca76a002225a687f6882110915864d5d
ioudahya <44835548+ioudahya@users.noreply.github.com>
2022-03-02 18:01:41 +0100

    Add files via upload

--- Wiki_QA_System-master/mysite/LMI_NLP/urls.py
+++ Wiki_QA_System-master/mysite/LMI_NLP/urls.py
@@ -9,12 +9,12 @@ app_name = 'LMI_NLP'
 # question_id -> pk , add as.view()
 urlpatterns = [
     # path('', views.IndexView.as_view(), name='index'),
-    path('', views.RexanaMain.as_view(), name='Rexana'),
+    #path('', views.RexanaMain.as_view(), name='Rexana'),
     # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('LMI_NLP/results.html', views.ResultsView.as_view(), name='results'),
     path('your-question.html',views.YourQuestion),
     # path('results/', views.ResultsView.as_view(), name='results'),
     # path('<int:question_id>/vote/', views.vote, name='vote'),
-
+    path('Rexana.html',views.Rexana),
     path('Execution.html', views.Execution)
 ]
